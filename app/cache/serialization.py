36197c365a79a444e9e3b0f5f86992721c91926f
vazik66 <vasilkov.dev@gmail.com>
2022-06-06 16:32:48 +0500

    Refactoring

--- app/cache/serialization.py
+++ app/cache/serialization.py
@@ -4,12 +4,12 @@ import json
 class JsonSerde(object):
     def serialize(self, key, value):
         if isinstance(value, str):
-            return value.encode('utf-8'), 1
-        return json.dumps(value).encode('utf-8'), 2
+            return value.encode("utf-8"), 1
+        return json.dumps(value).encode("utf-8"), 2
 
     def deserialize(self, key, value, flags):
         if flags == 1:
-            return value.decode('utf-8')
+            return value.decode("utf-8")
         if flags == 2:
-            return json.loads(value.decode('utf-8'))
+            return json.loads(value.decode("utf-8"))
         raise Exception("Unknown serialization format")
